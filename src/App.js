fbe95ebf3d043386d1e13756cc6a83859d366c38
TheDreamatorium <davidseesahaye@gmail.com>
2022-03-19 21:51:27 +0400

    use props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import Card from "./components/Card";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import "./style.scss";
+import cardImg from "./images/swimmer.png";
+
 
 export default function App() {
     return (
         <div>
-            <NavBar></NavBar>
-            <Hero></Hero>
-            <Card></Card>
+            <NavBar/>
+            <Hero/>
+            <div className="cards">
+                <Card
+                    img={cardImg}
+                    rating="5.0"
+                    reviewCount={6}
+                    country="USA"
+                    title="Life lessons with Katie Zaferes"
+                    price={136}
+                />
+                <Card
+                    img={cardImg}
+                    rating="5.0"
+                    reviewCount={6}
+                    country="USA"
+                    title="Life lessons with Katie Zaferes"
+                    price={136}
+                />
+            </div>
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,24 @@
-import cardImg from "../images/swimmer.png";
 import starImg from "../images/star.png";
 
-export default function Card() {
+export default function Card({
+    img, 
+    rating, 
+    reviewCount, 
+    country,
+    title,
+    price,
+}) {
     return (
         <section className="card-container">
-            <img className="card-img" src={cardImg} alt="card" />
+            <img className="card-img" src={img} alt="card" />
             <span className="card-status">SOLD OUT</span>
             <div className="card-info">
                 <div className="card-rating">
                     <img src={starImg} className="card-star" alt="star"/>
-                    <span className="card-rating-text">5.0 <span>(6).USA </span></span>
+                    <span className="card-rating-text">{rating} <span>({reviewCount}).{country} </span></span>
                 </div>
-                <label className="card-label">Life lessons with Katie Zaferes</label>
-                <label className="card-price"><span>From $136</span> / person</label>
+                <label className="card-label">{title}</label>
+                <label className="card-price"><span>From {price}</span> / person</label>
             </div>
         </section>
     );
